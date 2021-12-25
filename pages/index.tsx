46dff97a1c77f80fe674c226de5cd2374c0228d8
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-25 23:22:35 +0700

    create custom document

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <h1 className="bg-slate-500 text-white w-full py-10 text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="container bg-slate-500 ">
+      <h1 className=" text-white py-10 text-3xl  font-display underline">Hello world!</h1>;
+    </div>
+  );
 };
 
 export default Home;
