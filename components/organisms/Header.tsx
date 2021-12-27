23ec595b465e785b0d4f216891422b9759c28363
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-27 22:26:47 +0700

    add animation nav when out off ontop position

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,18 +1,33 @@
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 import { NavLinks, MobileNav } from '../molecules';
 
 export default function Header() {
+  const [onTop, setOnTop] = useState<boolean>(true);
+  const handleScroll = () => {
+    if (onTop !== (window.pageYOffset === 0)) {
+      setOnTop(window.pageYOffset === 0);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
   return (
-    <nav className="py-8 border-b-2 border-stone-400 border-dashed">
-      <ul className="flex items-center justify-between px-10 md:px-20 lg:px-36">
-        <li>
-          <Link href="/">
-            <a className="font-display text-4xl">ADC</a>
-          </Link>
-        </li>
-        <NavLinks />
-        <MobileNav />
-      </ul>
-    </nav>
+    <header className="fixed top-0 w-full">
+      <nav className={`py-8 transition-all duration-500 ${onTop ? '' : 'border-b-2 border-stone-400 border-dashed'} `}>
+        <ul className="flex items-center justify-between px-10 md:px-20 lg:px-36">
+          <li>
+            <Link href="/">
+              <a className="font-display text-4xl">ADC</a>
+            </Link>
+          </li>
+          <NavLinks />
+          <MobileNav />
+        </ul>
+      </nav>
+    </header>
   );
 }
