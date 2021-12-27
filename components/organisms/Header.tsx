bc9431ad09910507b1a264e53fe8ee26a4339584
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-27 22:37:28 +0700

    change navbar to sticky

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -16,12 +16,12 @@ export default function Header() {
     };
   });
   return (
-    <header className="fixed top-0 w-full">
-      <nav className={`py-8 transition-all duration-500 ${onTop ? '' : 'border-b-2 border-stone-400 border-dashed'} `}>
+    <header className="sticky top-0 w-full bg-light">
+      <nav className={`py-4 md:py-8 transition-all duration-500 ${onTop ? '' : 'border-b-2 border-stone-400 border-dashed'} `}>
         <ul className="flex items-center justify-between px-10 md:px-20 lg:px-36">
           <li>
             <Link href="/">
-              <a className="font-display text-4xl">ADC</a>
+              <a className="font-display text-2xl md:text-4xl">ADC</a>
             </Link>
           </li>
           <NavLinks />
