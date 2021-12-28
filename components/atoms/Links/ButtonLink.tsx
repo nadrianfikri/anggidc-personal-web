eb17eccde8d4f66f547d71db24d7989de607012c
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-29 00:21:13 +0700

    change style button link for primary variants

--- components/atoms/Links/ButtonLink.tsx
+++ components/atoms/Links/ButtonLink.tsx
@@ -11,12 +11,12 @@ export default function ButtonLink({ children, className = '', variants = 'prima
     <UnstyledLink
       {...rest}
       className={clsx(
-        'py-1 px-6 inline-block  font-bold hover:text-cyan-400 hover:-translate-y-1 animated-underline',
+        'py-1 px-6 inline-block  font-bold  hover:-translate-y-1 animated-underline',
         'border border-gray-600',
         'focus:outline-none focus-visible:text-primary-400',
         {
           'bg-darker text-white': variants === 'primary',
-          'bg-white text-darker hover:bg-darker hover:text-er focus-visible:text-dark': variants === 'secondary',
+          'bg-white text-darker hover:bg-darker hover:text-cyan-400 focus-visible:text-dark': variants === 'secondary',
         },
         className
       )}
