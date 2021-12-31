6bcb6f61d096fe379b32ca642df2da6f04b86fbd
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-31 17:20:34 +0700

    change responsive profile section md to lg breakpoint

--- components/organisms/Content.tsx
+++ components/organisms/Content.tsx
@@ -3,14 +3,14 @@ import { ButtonLink, CustomLink } from '../atoms';
 
 export default function Content() {
   return (
-    <div id="intro" className="px-10 block md:flex gap-10 relative h-auto md:h-80 pt-10 bg-darker ">
+    <div id="intro" className="px-10 block lg:flex gap-10 relative h-auto lg:h-80 pt-10 bg-darker ">
       <section className=" basis-1/4">
-        <div className="absolute -top-5 md:-top-[130px] left-0 w-36 md:w-[450px]">
+        <div className="absolute -top-5 lg:-top-[130px] left-0 w-36 lg:w-[450px]">
           <Image src="/images/nad-no-bg.png" alt="foto" width={450} height={450} layout="responsive" className="rounded-full bg-midDark md:rounded-none md:bg-transparent" />
         </div>
       </section>
-      <section className="flex-1 text-light flex flex-col md:grid  md:grid-rows-3   md:grid-flow-col  gap-4">
-        <p className="row-span-3 ml-24 md:ml-0 md:text-left text-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nulla suscipit facere ratione fuga doloribus.</p>
+      <section className="flex-1 text-light flex flex-col lg:grid  lg:grid-rows-3   lg:grid-flow-col  gap-4">
+        <p className="row-span-3 ml-24 lg:ml-0 lg:text-left text-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nulla suscipit facere ratione fuga doloribus.</p>
         <hr />
         <p className="col-span-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dicta exercitationem, rem perferendis corrupti deleniti iure non esse temporibus voluptatibus quae in itaque fugiat obcaecati.</p>
         <hr />
