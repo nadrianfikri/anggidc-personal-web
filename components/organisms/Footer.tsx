d0eb9961033c4c254da3d6103ac00d0a50daf1aa
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2022-05-01 23:40:43 +0700

    feat(components): get dynamic year on footer

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,4 +1,9 @@
-import { faInstagram, faLinkedin, faTwitter, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import {
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  IconDefinition,
+} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Col, Row, UnstyledLink } from '../atoms';
@@ -27,19 +32,31 @@ export default function Footer() {
     },
   ];
 
+  const getYear = () => {
+    const date = new Date();
+    return date.getFullYear();
+  };
+
   return (
     <footer className="px-10 lg:px-36 md-px-20 text-midDark text-center py-4">
       <Col className="justify-center items-center gap-y-4 border-t border-stone-400 py-10">
         <h1>Reach me out</h1>
         <Row className="gap-4">
           {sosmed.map(({ href, icon }) => (
-            <UnstyledLink openNewTab key={icon + href} href={href} className=" hover:text-darker hover:scale-110 transition-all ">
+            <UnstyledLink
+              openNewTab
+              key={icon + href}
+              href={href}
+              className=" hover:text-darker hover:scale-110 transition-all "
+            >
               <FontAwesomeIcon size="2x" icon={icon} />
             </UnstyledLink>
           ))}
         </Row>
       </Col>
-      <p className="text-xs md:text-sm text-darker">©2022 Anggi Dwi Cantika. All rights reserved.</p>
+      <p className="text-xs md:text-sm text-darker">
+        ©{getYear()} Anggi Dwi Cantika. All rights reserved.
+      </p>
     </footer>
   );
 }
