35ea4eccc2138cc90bb83534f370c8f1359a08b2
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2022-05-06 23:46:53 +0700

    feat(hooks): create useScroll to handle navbar visibillity

---
+++ hooks/useScroll.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+export const useScroll = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [bodyOffset, setBodyOffset] = useState(
+    document.body.getBoundingClientRect()
+  );
+
+  const [scrollY, setScrollY] = useState<number>(0);
+  const [scrollX, setScrollX] = useState<number>(0);
+  const [scrollDirection, setScrollDirection] = useState<string>('');
+
+  const handleScroll = () => {
+    setBodyOffset(document.body.getBoundingClientRect());
+    setScrollY(-bodyOffset.top);
+    setScrollX(bodyOffset.left);
+    setScrollDirection(scrollPosition > -bodyOffset.top ? 'down' : 'up');
+    setScrollPosition(-bodyOffset.top);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+
+  return {
+    scrollY,
+    scrollX,
+    scrollDirection,
+  };
+};
