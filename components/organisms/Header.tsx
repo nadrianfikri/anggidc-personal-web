50e73d0c3115ddf63b16d01b30a32dae9d91a188
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2022-05-07 00:29:52 +0700

    chore(components): use scroll hook implementationx

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,23 +1,28 @@
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
 import { NavLinks, MobileNav } from '../molecules';
+import { useScroll } from 'hooks/useScroll';
 
 export default function Header() {
-  const [onTop, setOnTop] = useState<boolean>(true);
-  const handleScroll = () => {
-    if (onTop !== (window.pageYOffset === 0)) {
-      setOnTop(window.pageYOffset === 0);
-    }
+  const { scrollY, scrollDirection } = useScroll();
+  const onTopScroll = scrollY < 10;
+
+  const styles = {
+    active: `visible `,
+    hidden: 'invisible -translate-y-[100%] ',
+    bottomBordered: 'border-b-2 border-stone-400 border-dashed',
   };
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  });
+
   return (
-    <header className="sticky z-10 top-0 w-full bg-light">
-      <nav className={`py-4 md:py-8 transition-all duration-300 ${onTop ? '' : 'border-b-2 border-stone-400 border-dashed'} `}>
+    <header
+      className={`sticky z-10 top-0 w-full bg-light transition-all duration-300 ease-in ${
+        scrollDirection === 'down' ? styles.active : styles.hidden
+      } `}
+    >
+      <nav
+        className={`py-4 md:py-8 transition-all duration-300 ${
+          onTopScroll ? '' : styles.bottomBordered
+        }`}
+      >
         <ul className="flex items-center justify-between px-10 md:px-20 lg:px-36">
           <li>
             <Link href="/">
