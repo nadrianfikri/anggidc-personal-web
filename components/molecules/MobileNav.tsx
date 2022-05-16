45907ca38c194fc3dd7e576e42c3f0653cb18723
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2022-05-16 13:09:42 +0700

    fix(components): handle mobile nav and disable scroll when menu is open

--- components/molecules/MobileNav.tsx
+++ components/molecules/MobileNav.tsx
@@ -1,11 +1,12 @@
 import { faStream, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Menu, Transition } from '@headlessui/react';
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 
 import { CustomLink } from '../atoms';
 
 export default function MobileNav() {
+  const [isOpen, setIsOpen] = useState(false);
   const links = [
     { href: '/Tentang', label: 'Tentang' },
     { href: '/Blog', label: 'Blog' },
@@ -15,15 +16,33 @@ export default function MobileNav() {
     { href: '/Kontak', label: 'Kontak' },
   ];
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    }
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isOpen]);
+
   return (
-    <Menu as="div" className="md:hidden inline-block font-medium ">
+    <Menu
+      as="div"
+      className="md:hidden inline-block font-medium "
+      onChange={(val: any) => console.log(val)}
+    >
       <Menu.Button className="relative z-20">
-        {({ open }) => (
-          <FontAwesomeIcon
-            className="text-darker/80 hover:scale-125 transition-all"
-            icon={open ? faTimes : faStream}
-          />
-        )}
+        {({ open }) => {
+          open ? setIsOpen(true) : setIsOpen(false);
+
+          return (
+            <FontAwesomeIcon
+              className="text-darker/80 hover:scale-125 transition-all"
+              icon={open ? faTimes : faStream}
+            />
+          );
+        }}
       </Menu.Button>
       <Transition
         as={Fragment}
@@ -36,7 +55,7 @@ export default function MobileNav() {
       >
         <Menu.Items
           as="ul"
-          className="absolute flex flex-col items-center justify-center top-0 right-0 w-3/4 h-screen bg-white/80 rounded-l-2xl shadow-xl gap-8 backdrop-blur-xl"
+          className="fixed flex flex-col items-center justify-center top-0 right-0 w-3/4 h-screen bg-white/80 rounded-l-2xl shadow-xl gap-8 backdrop-blur-xl"
         >
           <>
             {links.map(({ href, label }) => (
