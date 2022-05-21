d7c2688d0d8a37d5634c85279e0560bb32252111
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2022-05-22 00:36:22 +0700

    feat(pages): add text editor on dashboard

--- pages/adm-secret-777/dashboard.tsx
+++ pages/adm-secret-777/dashboard.tsx
@@ -1,5 +1,23 @@
-const dashboard = () => {
-  return <div>Halaman dashboard</div>;
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
+import Seo from '@/components/Seo';
+
+const TextEditor = dynamic(() => import('@/components/TextEditor'), {
+  ssr: false,
+});
+
+const Dashboard = () => {
+  const [value, setValue] = useState('');
+
+  console.log('value', value);
+
+  return (
+    <div className="container min-h-screen py-10">
+      <Seo title="Dashboard" />
+      <TextEditor value={value} onChange={setValue} />
+    </div>
+  );
 };
 
-export default dashboard;
+export default Dashboard;
